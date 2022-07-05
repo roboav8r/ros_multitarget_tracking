3ff0ea98ee6079472da28aec75614bf5bc7ddf44
John Duncan <john.a.duncan@utexas.edu>
2022-07-05 11:48:10 -0500

    Publish basic state info via custom msg

--- src/multitarget_tracking_node.cpp
+++ src/multitarget_tracking_node.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 
 #include "multitarget_tracking_node.h"
+#include "ros_multitarget_tracking/Gaussian2D.h"
+#include "ros_multitarget_tracking/GaussianMixture2D.h"
 
 // Initialize Gaussian Mixture component & belief
 GaussianMixtureComponent2D InitialGMComp;
@@ -14,23 +16,8 @@ GaussianMixtureComponent2D InitialGMComp;
  */
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+  // Initialize node
   ros::init(argc, argv, "multitarget_tracker");
-
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
 
   // With nodehandle created, populate initial belief GM with node's launch parameters
@@ -41,44 +28,29 @@ int main(int argc, char **argv)
   n.param<float> ("weight", InitialGMComp.weight, 1.0);
   Belief.push_back(InitialGMComp);
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  // Initialize publisher and messages
+  ros::Publisher state_pub = n.advertise<ros_multitarget_tracking::GaussianMixture2D>("multitarget_state", 1000);
+  ros_multitarget_tracking::GaussianMixture2D BeliefMsg;
+  ros_multitarget_tracking::Gaussian2D GaussianMsg;
 
   ros::Rate loop_rate(10);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
+  // Main loop
   while (ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    std_msgs::String msg;
 
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
+    // Populate state belief message with current Gaussians
+    BeliefMsg.models.clear();
+  
+    for (auto model : Belief) {
+      GaussianMsg.weight = model.weight;
+      GaussianMsg.mu_x = model.muX;
+      GaussianMsg.mu_y = model.muY;
+      GaussianMsg.sigma_x = model.sigmaX;
+      GaussianMsg.sigma_y = model.sigmaY;
+      BeliefMsg.models.push_back(GaussianMsg);
 
-    ROS_INFO("%s", msg.data.c_str());
+    } // for model : Belief
 
     /**
      * The publish() function is how you send messages. The parameter
@@ -86,12 +58,11 @@ int main(int argc, char **argv)
      * given as a template parameter to the advertise<>() call, as was done
      * in the constructor above.
      */
-    chatter_pub.publish(msg);
+    state_pub.publish(BeliefMsg);
 
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count;
   }
 
 
