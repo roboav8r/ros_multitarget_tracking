02a27fa89ba564817944e1f32d4d0a1fb0d1577d
John Duncan <john.a.duncan@utexas.edu>
2022-07-05 07:07:41 -0500

    Create multitarget tracker node and GM data structs

---
+++ src/multitarget_tracking_node.h
@@ -0,0 +1,20 @@
+#ifndef MULTITARGET_TRACKING_NODE_H_
+#define MULTITARGET_TRACKING_NODE_H_
+
+#include<vector>
+
+// 2D Gaussian Mixture component consisting of weight and first moments mu & sigma
+struct GaussianMixtureComponent2D {
+    float weight; // Weight of component
+    float muX; // Mean of GM in x-direction
+    float muY; // Mean of GM in y-direction
+    float sigmaX; // Variance of GM in x-direction
+    float sigmaY; // Variance of GM in y-direction
+};
+
+// Multiple weighted Gaussians together form a mixture
+struct GaussianMixture {
+    std::vector<GaussianMixtureComponent2D> models;
+};
+
+#endif  // FOO_BAR_BAZ_H_
