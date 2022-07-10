163f8afe8281433f33301c178c869d657cface30
John Duncan <john.a.duncan@utexas.edu>
2022-07-10 09:32:40 -0500

    Accept initial state estimate from .yaml file

--- src/multitarget_tracking_node.cpp
+++ src/multitarget_tracking_node.cpp
@@ -1,37 +1,60 @@
+#include <sstream>
+
+//#include <Eigen/Dense>
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
-#include <sstream>
 
 #include "multitarget_tracking_node.h"
 #include "ros_multitarget_tracking/Gaussian2D.h"
 #include "ros_multitarget_tracking/GaussianMixture2D.h"
 
-// Initialize Gaussian Mixture component & belief
-GaussianMixtureComponent2D InitialGMComp;
+// Debug independent assert statement
+#define ASSERT(x) if (not x) throw(std::exception())
 
-
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
+// Main loop
 int main(int argc, char **argv)
 {
-  // Initialize node
+  // Initialize ROS node
   ros::init(argc, argv, "multitarget_tracker");
   ros::NodeHandle n;
 
-  // With nodehandle created, populate initial belief GM with node's launch parameters
-  n.param<float> ("mu_x", InitialGMComp.muX, 0.0);
-  n.param<float> ("mu_y", InitialGMComp.muY, 0.0);
-  n.param<float> ("sigma_x", InitialGMComp.sigmaX, 0.5);
-  n.param<float> ("sigma_y", InitialGMComp.sigmaY, 0.5);
-  n.param<float> ("weight", InitialGMComp.weight, 1.0);
-  Belief.push_back(InitialGMComp);
+  // Get initial, user-supplied parameters from YAML file
+  n.getParam("mtt_node/x0", xInitial);
+  ASSERT(xInitial.getType() == XmlRpc::XmlRpcValue::TypeArray);
+  int NumGMs = xInitial.size();
+  std::cout << "Initializing state with " << NumGMs << " Gaussian Mixtures \n";
+  ROS_INFO("Initializing state with %i Gaussian Mixtures", NumGMs);
+
+  // Initialize tracker object with size = number of state vectors
+  GmPhdFilter gmPhd(4);
+
+  // Initialize GM component for populating the initial state belief
+  GaussianMixtureComponent gmComp;
+
+  // Populate tracker object's initial estimate with user-supplied value of x0
+  for (int ii=0; ii < xInitial.size() ; ii++) {
+    // Store each GM component from file in a loop variable
+    XmlRpc::XmlRpcValue gm = xInitial[ii];
+
+    // Assign gaussian mixture values from input file to temporary variable gmComp
+    gmComp.weight = (double)gm["weight"];
+    for (int jj = 0; jj < gm["mu"].size(); jj++) {
+      gmComp.state(jj) = gm["mu"][jj];
+      gmComp.variance(jj) = gm["sigma"][jj];
+    };
+
+    std::cout << "Got GM component with weight = " << gmComp.weight << ", state = " << gmComp.state << ", and variance = " << gmComp.variance << "\n"; 
+
+    // Add gmComp to the tracker's initial state belief vector
+    gmPhd.belief.GaussianMixtures.push_back(gmComp);
+
+  }
 
   // Initialize publisher and messages
-  ros::Publisher state_pub = n.advertise<ros_multitarget_tracking::GaussianMixture2D>("multitarget_state", 1000);
-  ros_multitarget_tracking::GaussianMixture2D BeliefMsg;
-  ros_multitarget_tracking::Gaussian2D GaussianMsg;
+  // ros::Publisher state_pub = n.advertise<ros_multitarget_tracking::GaussianMixture2D>("multitarget_state", 1000);
+  // ros_multitarget_tracking::GaussianMixture2D BeliefMsg;
+  // ros_multitarget_tracking::Gaussian2D GaussianMsg;
 
   ros::Rate loop_rate(10);
 
@@ -40,17 +63,17 @@ int main(int argc, char **argv)
   {
 
     // Populate state belief message with current Gaussians
-    BeliefMsg.models.clear();
+    // BeliefMsg.models.clear();
   
-    for (auto model : Belief) {
-      GaussianMsg.weight = model.weight;
-      GaussianMsg.mu_x = model.muX;
-      GaussianMsg.mu_y = model.muY;
-      GaussianMsg.sigma_x = model.sigmaX;
-      GaussianMsg.sigma_y = model.sigmaY;
-      BeliefMsg.models.push_back(GaussianMsg);
+    // for (auto model : Belief) {
+    //   GaussianMsg.weight = model.weight;
+    //   GaussianMsg.mu_x = model.muX;
+    //   GaussianMsg.mu_y = model.muY;
+    //   GaussianMsg.sigma_x = model.sigmaX;
+    //   GaussianMsg.sigma_y = model.sigmaY;
+    //   BeliefMsg.models.push_back(GaussianMsg);
 
-    } // for model : Belief
+    // } // for model : Belief
 
     /**
      * The publish() function is how you send messages. The parameter
@@ -58,7 +81,7 @@ int main(int argc, char **argv)
      * given as a template parameter to the advertise<>() call, as was done
      * in the constructor above.
      */
-    state_pub.publish(BeliefMsg);
+    // state_pub.publish(BeliefMsg);
 
     ros::spinOnce();
 
--- src/multitarget_tracking_node.h
+++ src/multitarget_tracking_node.h
@@ -3,16 +3,55 @@
 
 #include<vector>
 
-// 2D Gaussian Mixture component consisting of weight and first moments mu & sigma
-struct GaussianMixtureComponent2D {
+#include<Eigen/Dense>
+
+#include <XmlRpcValue.h>
+
+
+using std::vector;
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+
+// n-Dimensional Gaussian Mixture component consisting of weight and first moments mu & sigma
+struct GaussianMixtureComponent {
     float weight; // Weight of component
-    float muX; // Mean of GM in x-direction
-    float muY; // Mean of GM in y-direction
-    float sigmaX; // Variance of GM in x-direction
-    float sigmaY; // Variance of GM in y-direction
+    VectorXd state = VectorXd(4); // n-dimensional state vector
+    VectorXd variance = VectorXd(4); // n-dimensional variance vector
 };
 
-// A vector of weighted Gaussian components forms a mixture/belief of the current state
-std::vector<GaussianMixtureComponent2D> Belief;
+// A timestamped vector of weighted Gaussian Mixture components forms a mixture/belief of the current state
+struct StateEstimate {
+
+    std::vector<GaussianMixtureComponent> GaussianMixtures;
+};
+
+
+// Create class for tracker objects
+class GmPhdFilter {
+
+public:
+    // Constructor
+    GmPhdFilter(int numStateVars) 
+        : nStateVars(numStateVars), 
+        processTransition{MatrixXd::Zero(numStateVars,numStateVars)}, 
+        processCovariance{MatrixXd::Zero(numStateVars,numStateVars)}
+        //belief(numStateVars)
+    {
+        std::cout << "Created GM-PHD filter for system with " << this->nStateVars << " state variables \n";
+        ROS_INFO("Created GM-PHD filter for system with %i state variables", this->nStateVars);
+    };
+
+
+    int nStateVars{4}; //Number of dimensions in state vector
+    
+    StateEstimate belief; // Current state estimate/Belief
+
+private:
+    MatrixXd processTransition{4,4};
+    MatrixXd processCovariance{4,4};
+};
+
+// Initialize variable to store ROS parameter values
+XmlRpc::XmlRpcValue xInitial;
 
 #endif  // MULTITARGET_TRACKING_NODE_H_
